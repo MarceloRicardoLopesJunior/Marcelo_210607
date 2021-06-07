817863413d9182e7925f0f258b95d800b2d5986c
MarceloRicardoLopesJunior <84341271+MarceloRicardoLopesJunior@users.noreply.github.com>
2021-06-07 14:41:23 -0300

    Add files via upload

---
+++ Marcelo_210607.py
@@ -0,0 +1,25 @@
+for value in range(6):
+    print(value)
+print(list(range(3,21,4)))
+
+number = 4
+for value in range(800,1,-1):
+    #print("marcelo")
+    number = value
+    print(number)
+print(number)
+
+
+
+
+
+
+
+
+#names=["erik","kelly","rafael","maria","gabriela"]
+#for name in names:
+#    print(name)
+#    print(name.upper()+" "+ name)
+
+#    print(name.title())
+#print("hello")
